89659891e398fc2d5fa57249df8771bcea49e26c
Lemonaidsss <ramirezadrianfrancis@gmail.com>
2023-12-28 14:45:44 +0800

    updated config files for deployment

--- public-client/src/config/config.js
+++ public-client/src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   API_BASE_URL: "https://acidilabs-pharma-ims-server.onrender.com",
-  RESOURCES_BASE_URL: "hhttp://localhost:9000/",
+  RESOURCES_BASE_URL: "https://acidilabs-pharma-ims-resources-server.onrender.com",
 };
 
 export default config;
